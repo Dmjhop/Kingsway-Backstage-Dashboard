05c3e698063522d3f73ca273a38713db31f207f0
Dmjhop <dhscience2@gmail.com>
2024-02-01 14:10:01 -0500

    adding something

--- kingsway-backstage-dashboard/app/page.tsx
+++ kingsway-backstage-dashboard/app/page.tsx
@@ -12,6 +12,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Kingsway Worship Backstage</h1>
       <Link href={`/backstage-tv-display`}>Backstage View</Link>
     </main>
   )
