cc03d8d24e2d92e956e152aa125c5b7193c688a2
Dmjhop <dhscience2@gmail.com>
2024-02-08 10:28:09 -0500

    trying something out

--- kingsway-backstage-dashboard/app/page.tsx
+++ kingsway-backstage-dashboard/app/page.tsx
@@ -8,12 +8,12 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import Link from "next/link"
-import Navigation from "@/components/ui/Navigation"
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex">
       <h1>Kingsway Worship Backstage</h1>
+
       <Link href={`/backstage-tv-display`}>Backstage View</Link>
     </main>
   )
